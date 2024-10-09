06e643aa1ee11b073f1c5978baa5adce6f3cbd34
Giovanni-Lopez <alexisange001@gmail.com>
2024-10-08 22:45:33 -0600

    Se creo la clase conexion

---
+++ Clave3_Grupo4/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clave3_Grupo4
+{
+    class Conexion
+    {
+        //Se crearan las conexiones  a la base de datos nombre: Clave3_Grupo4DB
+
+    }
+}
