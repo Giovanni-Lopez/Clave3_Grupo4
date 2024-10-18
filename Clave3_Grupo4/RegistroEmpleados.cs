7fa2dfb2149ba4462ff7b0b09df5afb00a37a986
Giovanni-Lopez <alexisange001@gmail.com>
2024-10-17 22:30:44 -0600

    Se creo la clase 'RegistroEmpleados' donde se estaran desarrollando sus metodos o funciones

---
+++ Clave3_Grupo4/RegistroEmpleados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clave3_Grupo4
+{
+    class RegistroEmpleados
+    {
+
+        //Clase donde se crearan los metodos o funciones para los empleados
+
+    }
+}
