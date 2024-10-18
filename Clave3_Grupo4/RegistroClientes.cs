d71c95cfefaaf6c30e3d197b674b851d20615eb3
Giovanni-Lopez <alexisange001@gmail.com>
2024-10-17 23:26:24 -0600

    Se crearon algunos atributos que debe de tener un registro de clientes

--- Clave3_Grupo4/RegistroClientes.cs
+++ Clave3_Grupo4/RegistroClientes.cs
@@ -10,6 +10,18 @@ namespace Clave3_Grupo4
     {
 
         //Clase donde se crearan los metodos o funciones para los clientes
+        private string nombre;
+        private string apellidos;
+        private string dui;
 
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Dui { get => dui; set => dui = value; }
+
+        //public string Nombre
+        //{
+        //    get { return nombre; }
+        //    set { nombre = value; }
+        //}
     }
 }
