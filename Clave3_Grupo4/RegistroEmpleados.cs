9a5890112dbeb54adc16efffddaadc77d93e0b07
V9495 <157332278+V9495@users.noreply.github.com>
2024-10-23 08:37:37 -0600

    Cambios en la clase RegistroEmpleados

--- Clave3_Grupo4/RegistroEmpleados.cs
+++ Clave3_Grupo4/RegistroEmpleados.cs
@@ -11,5 +11,14 @@ namespace Clave3_Grupo4
 
         //Clase donde se crearan los metodos o funciones para los empleados
 
+        private string Nombre;
+        private string Apellidos;
+        private string Cargo;
+
+        public string nombre { get => Nombre; set => Nombre = value; }
+        public string apellidos { get => Apellidos; set => Apellidos = value; }
+        public string cargo { get => Cargo; set => Cargo = value; }
+
+
     }
 }
