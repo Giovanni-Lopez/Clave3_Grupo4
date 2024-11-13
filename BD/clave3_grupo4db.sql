0f8b27b7a59cb240f9eab6ab98d3ebab5980d458
V9495 <157332278+V9495@users.noreply.github.com>
2024-11-13 16:31:14 -0600

    Base de datos
    Se agrega la base de datos actualizada

--- BD/clave3_grupo4db.sql
+++ BD/clave3_grupo4db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 26, 2024 at 06:16 AM
+-- Generation Time: Nov 13, 2024 at 10:28 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -43,8 +43,19 @@ CREATE TABLE `clientes` (
   `idClientes` int NOT NULL,
   `nombres` varchar(100) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
-  `DUI` varchar(9) NOT NULL
+  `DUI` varchar(9) NOT NULL,
+  `estado` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `clientes`
+--
+
+INSERT INTO `clientes` (`idClientes`, `nombres`, `apellidos`, `DUI`, `estado`) VALUES
+(4, 'Alexis', 'Lopez', '909090', 'Activo'),
+(5, 'as', 'as', '121212121', 'Baja'),
+(6, 'ase', 'ase', '12', 'Activo'),
+(9, 'Vladimir', 'Cabrera', '3214123', 'Activo');
 
 -- --------------------------------------------------------
 
@@ -59,6 +70,14 @@ CREATE TABLE `empleados` (
   `tipo` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `empleados`
+--
+
+INSERT INTO `empleados` (`idEmpleados`, `nombres`, `apellidos`, `tipo`) VALUES
+(7, 'Alexis', 'Lopez', 'Gerente'),
+(8, 'Vladimir', 'Cabrera', 'Asesor');
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +89,48 @@ CREATE TABLE `productos` (
   `nombreProducto` varchar(50) NOT NULL,
   `tipoProducto` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipousuario`
+--
+
+CREATE TABLE `tipousuario` (
+  `idTipoUsuario` int NOT NULL,
+  `Nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tipousuario`
+--
+
+INSERT INTO `tipousuario` (`idTipoUsuario`, `Nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int NOT NULL,
+  `Usuario` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `idTipo` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `Usuario`, `Contraseña`, `Nombre`, `idTipo`) VALUES
+(1, 'qwe', '356a192b7913b04c54574d18c28d46e6395428ab', 'qw', 1),
+(2, 'Usuario', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Vladimir', 1),
+(3, 'm', '77de68daecd823babbb58edb1c8e14d7106e83bb', 'q', 1);
 
 --
 -- Indexes for dumped tables
@@ -102,6 +163,19 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`idproductos`);
 
 --
+-- Indexes for table `tipousuario`
+--
+ALTER TABLE `tipousuario`
+  ADD PRIMARY KEY (`idTipoUsuario`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idTipo` (`idTipo`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -109,19 +183,31 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idClientes` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idClientes` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `idEmpleados` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idEmpleados` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `productos`
 --
 ALTER TABLE `productos`
   MODIFY `idproductos` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tipousuario`
+--
+ALTER TABLE `tipousuario`
+  MODIFY `idTipoUsuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -133,6 +219,12 @@ ALTER TABLE `productos`
 ALTER TABLE `clienteproductos`
   ADD CONSTRAINT `clienteproductos_ibfk_1` FOREIGN KEY (`idClientes`) REFERENCES `clientes` (`idClientes`),
   ADD CONSTRAINT `clienteproductos_ibfk_2` FOREIGN KEY (`idProductos`) REFERENCES `productos` (`idproductos`);
+
+--
+-- Constraints for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`idTipo`) REFERENCES `tipousuario` (`idTipoUsuario`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
